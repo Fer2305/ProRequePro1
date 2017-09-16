ab6284579da54e259ea6bb26cfa002a0118e8cec
Fer2305 <fernando.chavarria2305@gmail.com>
2017-09-16 12:52:38 -0600

    Delete conn.php

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$db_name = "sql9194773";
-$mysql_username = "sql9194773";
-$mysql_password = "IXnCuYTkG5";
-$server_name = "sql9.freemysqlhosting.net";
-$conn = mysqli_connect($server_name, $mysql_username, $mysql_password,$db_name);
-?>
