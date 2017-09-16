94756459a2602b1382018762786c629be8726ecd
Fer2305 <fernando.chavarria2305@gmail.com>
2017-09-16 09:20:23 -0600

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php 
+$db_name = "sql9194773";
+$mysql_username = "sql9194773";
+$mysql_password = "IXnCuYTkG5";
+$server_name = "sql9.freemysqlhosting.net";
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_password,$db_name);
+?>
---
+++ login.php
@@ -0,0 +1,16 @@
+
+<?php 
+require "conn.php";
+//$user_name = "johnnys@estudiantes.cr";
+//$user_pass = "soysins";
+$user_name = $_POST["user_name"];
+$user_pass = $_POST["password"];
+$mysql_qry = "select * from estudiante where correo like '$user_name' and password like '$user_pass';";
+$result = mysqli_query($conn ,$mysql_qry);
+if(mysqli_num_rows($result) > 0) {
+echo "login success !!!!! Welcome user";
+}
+else {
+echo "login not success";
+}
+?>
